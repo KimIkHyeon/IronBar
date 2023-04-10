f763758175a0ab2efbe37b46accbf2895ae682f7
김익현/연구원/Audio Unit(ikhyeon.kim@lge.com) <ikhyeon.kim@lge.com>
2023-04-10 10:02:03 +0900

    Init Iron Bar

---
+++ IronBar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+char str[100000 + 10];//쇠막대기와 레이저의 배치를 나타내는 괄호 표현 문자열
+
+void InputData(){
+	cin >> str;
+}
+
+int main() {
+	int ans;
+	InputData();//입력
+	
+	//여기서부터 작성
+	
+	cout << ans << endl;//출력
+	return 0;
+}
